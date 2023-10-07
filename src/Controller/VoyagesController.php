8f137eaa884400cbdfb637bb3b4b9019635ca15b
vergerd <damien.verger@gmail.com>
2023-10-07 17:59:30 +0200

    Phase 8 : ORM (affichage en colonne des informations provenant de la BDD)

--- src/Controller/VoyagesController.php
+++ src/Controller/VoyagesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\VisiteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,10 +14,26 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class VoyagesController extends AbstractController{
     /**
+     * 
+     * @var VisiteRepository
+     */
+    private $repository;
+    /**
+     * 
+     * @param VisiteRepository $repository
+     */
+    public function __construct(VisiteRepository $repository){
+        $this->repository = $repository;
+    }
+    
+    /**
      * @Route("/voyages", name = "voyages")
      * @return response
      */
     public function index() : Response{
-        return $this->render("pages/voyages.html.twig");
+        $visites = $this->repository->findAll();
+        return $this->render("pages/voyages.html.twig", [
+            'visites' => $visites
+        ]);
     }
 }
--- src/Entity/Visite.php
+++ src/Entity/Visite.php
@@ -85,6 +85,15 @@ class Visite
     {
         return $this->datecreation;
     }
+    
+    public function getDatecreationString() : string
+    {
+        if($this->datecreation == null){
+            return"";
+        }else {
+            return $this->datecreation->format('d/m/Y');
+        }
+    }
 
     public function setDatecreation(?\DateTimeInterface $datecreation): self
     {
